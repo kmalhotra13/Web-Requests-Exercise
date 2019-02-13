bb80f671cacb7ce2bddbce78f68ff0b02bd97b7b
kmalhotra13 <k9@sukarm.com>
2019-02-13 17:59:16 -0500

    Update challenge_3.py

--- challenge_3.py
+++ challenge_3.py
@@ -5,3 +5,23 @@
 
 # GOAL: Calculate the average grade from the link
 
+import json
+import requests
+
+request_url = "https://raw.githubusercontent.com/prof-rossetti/georgetown-opim-243-201901/master/data/gradebook.json"
+response = requests.get(request_url)
+
+print(response.status_code)
+print(type(response.status_code))
+
+print(response.text)
+print(type(response))
+
+# response_text = response.text
+
+# parsed_response = json.loads(response_text)
+
+# print(parsed_response)
+# print(type(parsed_response))
+
+# print("The name of the product is: " + parsed_response["name"])
