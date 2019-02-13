9e63d86a18212d43e7e8cf58c95385971cca991a
kmalhotra13 <k9@sukarm.com>
2019-02-13 17:58:03 -0500

    Create challenge_3.py

---
+++ challenge_3.py
@@ -0,0 +1,7 @@
+# challenge 3
+
+# Write a Python program which issues a GET request for this gradebook.json data, then calculate and print the average, min, and max grades.
+# https://raw.githubusercontent.com/prof-rossetti/georgetown-opim-243-201901/master/data/gradebook.json
+
+# GOAL: Calculate the average grade from the link
+
