cb33c74718994bd0fe8a33b39ff9ecc84287e203
kmalhotra13 <k9@sukarm.com>
2019-02-14 09:38:39 -0500

    Update challenge_2.py

--- challenge_2.py
+++ challenge_2.py
@@ -1,2 +1,7 @@
-#web-requests.py
+# challenge 2
+
+# Write a Python program which issues a GET request for this products.json data, then loop through each product and print its "name" and "id".
+# https://raw.githubusercontent.com/prof-rossetti/georgetown-opim-243-201901/master/data/products.json
+# 
+
 
