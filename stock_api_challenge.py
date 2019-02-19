6ac1762fb767f4bdaf38b3ce3b2d5138059d7678
kmalhotra13 <k9@sukarm.com>
2019-02-19 17:39:12 -0500

    Create stock_api_challenge.py

---
+++ stock_api_challenge.py
@@ -0,0 +1,18 @@
+# Stock Price Challenge
+
+# goal: print the latest closing price
+
+import json
+import requests
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=_____________"
+
+response = requests.get(request_url)
+
+print("RESPONSE STATUS: " + str(response.status_code))
+print("RESPONSE TEXT: " + response.text)
+
+parsed_response = json.loads(response.text)
+
+print("THE LATEST CLOSING PRICE IS: $XYZ.00")
+print("THE AVERAGE GRADE IS: " + str(avg_grade))
